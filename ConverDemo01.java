92811d3a626bbbd3a70498c7e29e4fcbe1a74b9b
shihunzunhu <787819308@qq.com>
2014-08-18 16:23:43 +0800

    Initial commit

---
+++ ConverDemo01.java
@@ -0,0 +1,42 @@
+package com.bjsxt.io.convert;
+
+import java.io.UnsupportedEncodingException;
+
+public class ConverDemo01 {
+
+	/**
+	 * @param args
+	 * @throws UnsupportedEncodingException 
+	 */
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		String str ="?§Û?";
+		byte[] data =str.getBytes();
+		//?????????
+		System.out.println(new String(data,0,3));
+		
+		
+		
+	}
+	/**
+	 * ????????????????????????????
+	 * @throws UnsupportedEncodingException 
+	 */
+	public static void test1() throws UnsupportedEncodingException{
+		//???? byte -->char
+		String str ="?§Û?"; //gbk 
+		//???? char -->byte
+		byte[] data =str.getBytes();
+		//??????????????
+		System.out.println(new String(data));
+		data =str.getBytes("utf-8"); //?Ú…???????
+		//????????????
+		System.out.println(new String(data));
+		
+		//????
+		byte[] data2 = "?§Û?".getBytes("utf-8");
+		//????
+		str=new String(data2,"utf-8");
+		System.out.println(str);
+	}
+
+}
